84124469c8794109e75680cd9c3a14849c71ffb5
saivishalvangala <saivishalvangala@gmail.com>
2019-10-11 10:52:54 +0530

    first commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,10 +1,13 @@
 package com.stackroute;
+import com.stackroute.config.Config;
 import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import com.stackroute.demo.BeanLifecycleDemoBean;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,9 +16,12 @@ public class Main {
 //        Movie movie1=(Movie)factory.getBean("movie");
 //        movie1.display();
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         Movie movie = context.getBean("movie",Movie.class);
         System.out.println(movie);
+
+        BeanLifecycleDemoBean beanLifecycleDemoBean = context.getBean(BeanLifecycleDemoBean.class);
+        context.registerShutdownHook();
 
 
     }
--- src/main/java/com/stackroute/config/Config.java
+++ src/main/java/com/stackroute/config/Config.java
@@ -4,6 +4,8 @@ import com.stackroute.domain.Actor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import com.stackroute.demo.BeanLifecycleDemoBean;
+
 
 @Configuration
 @ComponentScan("com.stackroute.domain")
@@ -27,5 +29,12 @@ public class Config {
     public Actor actor3() {
         Actor actor = new Actor("vishal","Male",30);
         return actor;
+    }
+
+    @Bean(initMethod = "customInit",destroyMethod = "customDestroy")
+    public BeanLifecycleDemoBean beanLifecycleDemoBean()
+    {
+        BeanLifecycleDemoBean beanLifecycleDemoBean=new BeanLifecycleDemoBean();
+        return beanLifecycleDemoBean;
     }
 }
